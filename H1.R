c08c7d4a69ad520f4e9ee1be4875efffb84a1221
Javier Alvarado <77702824+XaviAlvarado18@users.noreply.github.com>
2024-01-27 19:22:08 -0600

    Ejercicio 4.2

--- H1.R
+++ H1.R
@@ -158,8 +158,13 @@ print(top_10_presupuesto)
 
 #4.2 Peliculas con mayores ingresos
 
+datos_ordenados_ingresos <- movies[order(-movies$revenue), ]
 
+# Seleccionar las 10 primeras filas con solo las columnas de interés
+top_10_ingresos <- head(datos_ordenados_ingresos[, c("revenue", "title")], 10)
 
+# Imprimir el resultado
+print(top_10_ingresos)
 
 
 #4.3
