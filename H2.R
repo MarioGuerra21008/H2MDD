ae6523838d2b386c8568dd2fb95eda512322394c
Javier Alvarado <77702824+XaviAlvarado18@users.noreply.github.com>
2024-02-08 17:45:48 -0600

    Normalizar la data

--- H2.R
+++ H2.R
@@ -1,5 +1,10 @@
+
 movies<-read.csv("movies.csv")
 View(movies)
+
+####
+#### Preprocesamiento de datos para el Clustering
+####
 
 # Dividir la columna 'actorsPopularity' en columnas separadas
 actors_popularity <- strsplit(movies$actorsPopularity, "|", fixed = TRUE)
@@ -14,8 +19,18 @@ movies_clust_data <- movies[, c("budget", "revenue", "runtime", "actorsPopularit
                                 "voteAvg", "voteCount", "genresAmount", "productionCoAmount", "productionCountriesAmount", 
                                 "actorsAmount", "castWomenAmount", "castMenAmount")]
 
-movies_clust_data <- na.omit(movies_clust_data)  # Eliminar filas con valores faltantes
+movies_clust_data <- na.omit(movies_clust_data) # Eliminar filas con valores faltantes
+
+#Eliminar los valores no numericos
+movies_clust_data <- movies_clust_data[!is.na(as.numeric(movies_clust_data$castWomenAmount)) &
+                                         !is.na(as.numeric(movies_clust_data$castMenAmount)), ]
+
+# Convertir las columnas a tipo numérico
+movies_clust_data$castWomenAmount <- as.numeric(movies_clust_data$castWomenAmount)
+movies_clust_data$castMenAmount <- as.numeric(movies_clust_data$castMenAmount)
+
+
+str(movies_clust_data)
 
 View(movies_clust_data)
-
 normalized_data_movies <- scale(movies_clust_data)
