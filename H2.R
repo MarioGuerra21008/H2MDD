70d5a68746c13b8e795620ff863d172d1467307d
Javier Alvarado <77702824+XaviAlvarado18@users.noreply.github.com>
2024-02-10 16:26:24 -0600

    Algoritmo K-Means

--- H2.R
+++ H2.R
@@ -24,6 +24,8 @@ movies_clust_data <- na.omit(movies_clust_data) # Eliminar filas con valores fal
 #Eliminar los valores no numericos
 movies_clust_data <- movies_clust_data[!is.na(as.numeric(movies_clust_data$castWomenAmount)) &
                                          !is.na(as.numeric(movies_clust_data$castMenAmount)), ]
+rm(movies_clust_data)
+View(movies_clust_data)
 
 # Convertir las columnas a tipo numérico
 movies_clust_data$castWomenAmount <- as.numeric(movies_clust_data$castWomenAmount)
@@ -32,5 +34,20 @@ movies_clust_data$castMenAmount <- as.numeric(movies_clust_data$castMenAmount)
 
 str(movies_clust_data)
 
-View(movies_clust_data)
 normalized_data_movies <- scale(movies_clust_data)
+View(normalized_data_movies)
+
+set.seed(123)
+k_values <- 1:10
+iner <- numeric(length(k_values))
+
+for (k in k_values) {
+  model <- kmeans(normalized_data_movies, centers = k)
+  iner[k] <- model$tot.withinss
+}
+
+# Graficar la curva de codo
+plot(k_values, iner, type = "b", main = "Método del Codo", xlab = "Número de Clústeres (k)", ylab = "Inercia")
+
+# Agregar líneas para facilitar la identificación del codo
+abline(v = which.min(diff(iner) > 10) + 1, col = "red", lty = 2)
